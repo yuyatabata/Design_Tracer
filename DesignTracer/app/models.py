df7eb66c126f81c70793e5927e75090b1609ff22
田端佑也 <tabayu5@gmail.com>
2020-01-06 09:19:11 +0900

    fix image

--- DesignTracer/app/models.py
+++ DesignTracer/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Image(models.Model):
     title = models.CharField(max_length=150)
     comment = models.TextField(blank=True)
-    image = models.ImageField(upload_to='../images')
+    image_f = models.ImageField(upload_to='../images')
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
