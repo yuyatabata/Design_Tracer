ae7999f4c3f36a7cf0966ae4615488a31645f609
田端佑也 <tabayu5@gmail.com>
2020-01-09 19:24:27 +0900

    fix float

--- DesignTracer/app/forms.py
+++ DesignTracer/app/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Image
 
@@ -5,3 +6,8 @@ class ImageForm(ModelForm):
     class Meta:
         model = Image
         fields = ['title', 'comment']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'タイトル'}),
+            'comment': forms.Textarea(
+                attrs={'placeholder': 'コメント'}),
+        }
