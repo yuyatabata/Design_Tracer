f5de11d3bd166762d9d24d50834a08373c305888
田端佑也 <tabayu5@gmail.com>
2020-01-11 11:11:30 +0900

    fix css

--- DesignTracer/app/urls.py
+++ DesignTracer/app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('users/<int:pk>/',views.users_detail, name='users_detail'),
     path('login/',auth_views.LoginView.as_view(template_name="app/login.html"), name="login"),
-    path('logout/',auth_views.LoginView.as_view(), name="logout"),
+    path('logout/',auth_views.LogoutView.as_view(), name="logout"),
     path('signup/', views.signup, name='signup'),
     path('images/<int:pk>/', views.images_detail, name="images_detail"),
     path('images/<int:pk>/delete/', views.images_delete, name="images_delete"),
